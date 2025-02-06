01c7d7ad25fdb27ea042b791c42de4b9e2484544
Alex Lin <a68lin@HarmonyOS.local>
2025-02-06 13:40:57 -0500

    Add test-sample.sql and .out

--- BackEnd/Database/testdb/test-sample.sql
+++ BackEnd/Database/testdb/test-sample.sql
@@ -1,5 +1,35 @@
---Filter jobs based on min salary, workType, city
+--1. Users can filter jobs based on min salary, workType, city, company
+SELECT Title, Description, MinSalary, MaxSalary, WorkType, (SELECT CityName FROM Cities WHERE CityId = j.CityId) AS location
+FROM JobPostings J
+JOIN Employers E ON E.UserId = J.EmployerId
+JOIN Companies Com ON Com.CompanyId = E.CompanyId
+JOIN Cities Cit ON Cit.CityId = J.CityId
+WHERE MinSalary >= 60000 AND workType = "Full-time" AND Cit.CityName = "New York" AND Com.CompanyName == "Google";
 
---Employer can add new job postings
+--Employees can see company's profile (name, location, industry, Size)
+SELECT CompanyName, (SELECT CityName FROM Cities WHERE CityId = Com.CityId) AS location, (SELECT IndustryName FROM Industry WHERE IndustryId = F.IndustryId) AS Industry, Size
+FROM Companies Com
+JOIN FocusOn F ON Com.CompanyId = F.CompanyId
+Where Com.CompanyName == "Google";
 
---User can apply/Dislike/Shortlist job postings
+--Employers can post new job postings
+INSERT INTO JobPostings (EmployerId, Title, Description, MinSalary, MaxSalary, WorkType, CityId, IsActive, PostDate)
+VALUES (2, 'Product Manager', 'manage products', 90000, 150000, "Full-time", 1, 1, CURRENT_DATE);
+SELECT * FROM JobPostings;
+
+--Employees can apply/Dislike/Shortlist job postings
+INSERT INTO Applications (EmployeeId, JobId, status)
+VALUES (1, 1, 'Pending');
+SELECT * FROM Applications;
+
+--Employees can view all their applications/Dislikes/Shortlists
+SELECT J.Title, J.Description, J.IsActive, A.ApplyDate, A.Status
+FROM Applications A
+JOIN JobPostings J ON A.JobId = J.JobId
+WHERE A.EmployeeId = 1;
+
+--Employees can manage its profile (location, resume)
+UPDATE Employees
+SET ResumeUrl = 'resumes/john_doe.pdf'
+WHERE UserId = 1;
+SELECT * FROM Employees;
