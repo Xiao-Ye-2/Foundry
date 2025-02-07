d23e507382c9f9d771716206fd3fdb33c1ffb43c
Alex Lin <104092094+a68lin@users.noreply.github.com>
2025-02-07 10:15:52 -0500

    Update test-sample.sql

--- BackEnd/Database/testdb/test-sample.sql
+++ BackEnd/Database/testdb/test-sample.sql
@@ -6,29 +6,29 @@ JOIN Companies Com ON Com.CompanyId = E.CompanyId
 JOIN Cities Cit ON Cit.CityId = J.CityId
 WHERE MinSalary >= 60000 AND workType = "Full-time" AND Cit.CityName = "New York" AND Com.CompanyName == "Google";
 
---Employees can see company's profile (name, location, industry, Size)
+--2. Employees can see company's profile (name, location, industry, Size)
 SELECT CompanyName, (SELECT CityName FROM Cities WHERE CityId = Com.CityId) AS location, (SELECT IndustryName FROM Industry WHERE IndustryId = F.IndustryId) AS Industry, Size
 FROM Companies Com
 JOIN FocusOn F ON Com.CompanyId = F.CompanyId
 Where Com.CompanyName == "Google";
 
---Employers can post new job postings
+--3. Employers can post new job postings
 INSERT INTO JobPostings (EmployerId, Title, Description, MinSalary, MaxSalary, WorkType, CityId, IsActive, PostDate)
 VALUES (2, 'Product Manager', 'manage products', 90000, 150000, "Full-time", 1, 1, CURRENT_DATE);
 SELECT * FROM JobPostings;
 
---Employees can apply/Dislike/Shortlist job postings
+--4. Employees can apply/Dislike/Shortlist job postings
 INSERT INTO Applications (EmployeeId, JobId, status)
 VALUES (1, 1, 'Pending');
 SELECT * FROM Applications;
 
---Employees can view all their applications/Dislikes/Shortlists
+--5. Employees can view all their applications/Dislikes/Shortlists
 SELECT J.Title, J.Description, J.IsActive, A.ApplyDate, A.Status
 FROM Applications A
 JOIN JobPostings J ON A.JobId = J.JobId
 WHERE A.EmployeeId = 1;
 
---Employees can manage its profile (location, resume)
+--6. Employees can manage its profile (location, resume)
 UPDATE Employees
 SET ResumeUrl = 'resumes/john_doe.pdf'
 WHERE UserId = 1;
